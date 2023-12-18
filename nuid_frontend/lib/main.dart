4084236a94dc572a5f529a61ab4be689f931fdba
Harsh Soni <algebra.soni@gmail.com>
2023-12-18 17:44:38 +0000

    add container like PhonePe

--- nuid_frontend/lib/main.dart
+++ nuid_frontend/lib/main.dart
@@ -25,7 +25,7 @@ class MyHomePage extends StatelessWidget {
             icon: Icon(
               Icons.payment,
               size: 28.0,
-              color: Colors.white,
+              color: Colors.purple,
             ),
             onPressed: () {
               // Handle Pay button click
@@ -35,7 +35,7 @@ class MyHomePage extends StatelessWidget {
             icon: Icon(
               Icons.qr_code,
               size: 28.0,
-              color: Colors.white,
+              color: Colors.purple,
             ),
             onPressed: () {
               // Handle QR code button click
@@ -44,25 +44,79 @@ class MyHomePage extends StatelessWidget {
         ],
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Enter payment amount',
-                ),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(16.0),
+              alignment: Alignment.centerLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Transfer Crypto',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ClickableIcon(
+                        icon: Icons.phone,
+                        label: 'To Mobile Number',
+                        onPressed: () {
+                          // Handle To Mobile Number click
+                        },
+                      ),
+                      ClickableIcon(
+                        icon: Icons.account_balance_wallet,
+                        label: 'To Wallet Address',
+                        onPressed: () {
+                          // Handle To Wallet Address click
+                        },
+                      ),
+                      ClickableIcon(
+                        icon: Icons.account_box,
+                        label: 'To Self Account',
+                        onPressed: () {
+                          // Handle To Self Account click
+                        },
+                      ),
+                      ClickableIcon(
+                        icon: Icons.account_balance,
+                        label: 'Check Balance',
+                        onPressed: () {
+                          // Handle Check Balance click
+                        },
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () {
-                  // Handle Phrycto Pay button click
-                },
-                child: Text('Phrycto Pay'),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Enter payment amount',
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle Phrycto Pay button click
+                    },
+                    child: Text('Phrycto Pay'),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
       bottomNavigationBar: BottomAppBar(
@@ -101,6 +155,39 @@ class MyHomePage extends StatelessWidget {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class ClickableIcon extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onPressed;
+
+  const ClickableIcon({
+    required this.icon,
+    required this.label,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Column(
+        children: [
+          Icon(
+            icon,
+            size: 40.0,
+            color: Theme.of(context).primaryColor,
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            label,
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
