1f04de038e8169d9d90dc13c464dab06d3bf5808
Harsh Soni <algebra.soni@gmail.com>
2023-12-21 13:01:18 +0000

    distinguish footbar className

--- react_ui/nuid_pay/src/components/FootBar.js
+++ react_ui/nuid_pay/src/components/FootBar.js
@@ -1,22 +1,32 @@
 // src/components/FootBar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faExchangeAlt, faBalanceScale, faHistory } from '@fortawesome/free-solid-svg-icons';
 import './FootBar.css'; // You can style this file accordingly
 
 const FootBar = () => {
     return (
         <div className="foot-bar">
             <Link to="/">
-                <div className="icon">Home</div>
+                <div className="foot-icon-container">
+                    <FontAwesomeIcon icon={faHome} className="foot-icon" />
+                </div>
             </Link>
             <Link to="/transactions">
-                <div className="icon">Transactions</div>
+                <div className="foot-icon-container">
+                    <FontAwesomeIcon icon={faExchangeAlt} className="foot-icon" />
+                </div>
             </Link>
             <Link to="/balance">
-                <div className="icon">Balance</div>
+                <div className="foot-icon-container">
+                    <FontAwesomeIcon icon={faBalanceScale} className="foot-icon" />
+                </div>
             </Link>
             <Link to="/history">
-                <div className="icon">History</div>
+                <div className="foot-icon-container">
+                    <FontAwesomeIcon icon={faHistory} className="foot-icon" />
+                </div>
             </Link>
         </div>
     );
